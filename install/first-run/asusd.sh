6bb8bed320fbd5c2c36b7f6ef34984a28b4867a9
Mohamedsayhii <Mohamedsayhii@users.noreply.github.com>
2026-06-07 18:48:55 +0100

    Cap battery at 85

--- install/first-run/asusd.sh
+++ install/first-run/asusd.sh
@@ -2,5 +2,5 @@ if command -v asusctl &>/dev/null; then
     sudo mkdir -p /etc/asusd
     sudo systemctl reset-failed asusd.service
     sudo systemctl restart asusd.service
-    asusctl battery limit 80
+    asusctl battery limit 85
 fi
